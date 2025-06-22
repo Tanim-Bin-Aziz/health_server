4d696fbe0725f421fa17331b67121d35c234c444
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-23 00:54:36 +0600

    project setup done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import { Server } from "http";
 import app from "./app";
 
 const port = 3000;
-app.listen(port, () => {
-  console.log("app is running on port", port);
-});
+
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log("server is running on port", port);
+  });
+}
+
+main();

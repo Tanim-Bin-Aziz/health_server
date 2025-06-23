2cd56ec608a6e4fc61a38537443fee65cc6b7a31
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-23 12:28:58 +0600

    hashed password with bcrypt

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { userRoute } from "./app/modules/User/user";
+import { userRoute } from "./app/modules/User/user.routes";
 const app: Application = express();
 
 app.use(cors());
-
+//parser added
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send({
     Message: "health care server..",

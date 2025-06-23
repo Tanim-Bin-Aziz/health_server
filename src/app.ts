2080253050aba6275477704bb696d5054d2ed140
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-23 20:38:19 +0600

    data get via admin routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { userRoute } from "./app/modules/User/user.routes";
+import { AdminRoutes } from "./app/modules/Admin/admin.routes";
+
 const app: Application = express();
 
 app.use(cors());
@@ -14,5 +16,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/admin", AdminRoutes);
 
 export default app;

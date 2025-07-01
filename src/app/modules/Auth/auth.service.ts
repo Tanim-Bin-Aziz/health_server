58504058bed0004affa056598a8c2fb436650c51
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-02 00:11:49 +0600

    Generating Access Tokens for User Login

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,32 @@
+import prisma from "../../../shared/prisma";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log("user logged in....", payload);
+  const userData = await prisma.user.findFirstOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcd",
+    {
+      algorithm: "HS256",
+      expiresIn: "15m",
+    }
+  );
+
+  console.log({ accessToken });
+  return userData;
 };
 
 export const AuthServices = {

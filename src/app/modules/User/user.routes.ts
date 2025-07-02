0d0f20154f23b2889fd7dd085de95f2abacd2c50
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-02 17:22:41 +0600

    Creating Reusable Authentication Middleware and Handling API Errors

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,35 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
-import { jwtHelpers } from "../../../helpars/jwtHelpers";
-import config from "../../../config";
-import { Secret } from "jsonwebtoken";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
-
-const auth = (...roles: string[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers.authorization;
-
-      if (!token) {
-        throw new Error("You are not authorized!");
-      }
-
-      const verifiedUser = jwtHelpers.verifyToken(
-        token,
-        config.jwt.jwt_secret as Secret
-      );
-      console.log(verifiedUser);
-
-      if (roles.length && !roles.includes(verifiedUser.role)) {
-        throw new Error("You are not authorized!");
-      }
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
 

fed792e41161487914eee77716e6bf4e0e94adc6
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-02 17:44:59 +0600

    Implementing Role-Based Access Control on API Routes

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,19 +1,37 @@
 import express from "express";
 import { AdminController } from "./admin.controller";
-
 import validateRequest from "../../middlewares/validateRequest";
 import { adminValidationsSchemas } from "./admin.validations";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma";
 
 const router = express.Router();
 
-router.get("/", AdminController.getAllFromDB);
-router.get("/:id", AdminController.getByIdFromDB);
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getAllFromDB
+);
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getByIdFromDB
+);
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(adminValidationsSchemas.update),
   AdminController.updateIntoDB
 );
-router.delete("/:id", AdminController.deleteFromDb);
-router.delete("/soft/:id", AdminController.softDeleteFromDb);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteFromDb
+);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.softDeleteFromDb
+);
 
 export const AdminRoutes = router;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,9 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma";
 
 const router = express.Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
+router.post(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  userController.createAdmin
+);
 
 export const userRoute = router;

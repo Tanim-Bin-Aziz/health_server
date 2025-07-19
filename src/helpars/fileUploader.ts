f937af54254fa8299f5b82431dd2a6ebbf812be7
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-19 19:18:37 +0600

    file upload working

--- src/helpars/fileUploader.ts
+++ src/helpars/fileUploader.ts
@@ -1,6 +1,14 @@
 import multer from "multer";
 import path from "path";
 
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dshyf27co",
+  api_key: "743122863663612",
+  api_secret: "nVuM171YNyy7ex0JzmukJhDZsdI",
+});
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(process.cwd(), "uploads"));
@@ -11,6 +19,18 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+const uploadToCloudinary = async (file: any) => {
+  cloudinary.uploader.upload(
+    "D:/Software/project_health/uploads/_titan eren yeager.jpg",
+    { public_id: "user_picture" },
+    function (error, result) {
+      console.log(result);
+    }
+  );
+};
+
 export const fileUploader = {
   upload,
+  uploadToCloudinary,
 };

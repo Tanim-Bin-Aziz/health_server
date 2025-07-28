e4b98609f92a83d8822d1b2b0b90379b37ad9acc
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-28 15:02:43 +0600

    doctor create data handle sucessfully working

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -17,7 +17,24 @@ const createAdmin = async (req: Request, res: Response, next: unknown) => {
     });
   }
 };
+const createDoctor = async (req: Request, res: Response, next: unknown) => {
+  try {
+    const result = await userService.createDoctor(req);
+    res.status(200).json({
+      success: true,
+      message: "Doctor created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err instanceof Error ? err.name : "something went wrong",
+      error: err,
+    });
+  }
+};
 
 export const userController = {
   createAdmin,
+  createDoctor,
 };

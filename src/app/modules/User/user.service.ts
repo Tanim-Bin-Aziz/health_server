0fade49fc9b7d3a54ca5442f8dae0890f5ad06c4
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-07-30 20:37:50 +0600

    few error and bugs solved

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -226,41 +226,47 @@ const getMyProfile = async (user) => {
   return { ...userInfo, ...profileInfo };
 };
 
-const updateMyProfile = async (user, payload) => {
+const updateMyProfile = async (user: any, req: Request) => {
   const userInfo = await prisma.user.findUniqueOrThrow({
     where: {
       email: user.email,
       status: UserStatus.ACTIVE,
     },
   });
+  const file = req.file as IFile;
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+    req.body.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
   let profileInfo;
   if (userInfo.role === UserRole.SUPER_ADMIN) {
     profileInfo = await prisma.admin.update({
       where: {
         email: userInfo.email,
       },
-      data: payload,
+      data: req.body,
     });
   } else if (userInfo.role === UserRole.ADMIN) {
     profileInfo = await prisma.admin.update({
       where: {
         email: userInfo.email,
       },
-      data: payload,
+      data: req.body,
     });
   } else if (userInfo.role === UserRole.DOCTOR) {
     profileInfo = await prisma.doctor.update({
       where: {
         email: userInfo.email,
       },
-      data: payload,
+      data: req.body,
     });
   } else if (userInfo.role === UserRole.PATIENT) {
     profileInfo = await prisma.patient.update({
       where: {
         email: userInfo.email,
       },
-      data: payload,
+      data: req.body,
     });
   }
   return { ...profileInfo };
